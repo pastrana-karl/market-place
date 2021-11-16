b92ebf4a95fd4ac8be3f322bc9f053da892bb2f1
Karl <pastranakd@gmail.com>
2021-11-16 15:52:53 +0800

    Sixth commit for market place

--- includes/constants.php
+++ includes/constants.php
@@ -2,17 +2,15 @@
     session_start();
 	ob_start();
 
-    define('SITEURL','https://market-p.herokuapp.com/');
-    // define('LOCALHOST', 'localhost');
-    // define('DB_USERNAME', 'root');
-    // define('DB_PASSWORD', '');
-    // define('DB_NAME', 'mp_db');
-
-
-    // $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-    // if (!$conn){
-    //     die("Connection failed: " . mysqli_connect_error());
-    // }
-
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
