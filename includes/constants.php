ccdf04ef38a66dc13a81980661c92b84ad7ebd59
Karl <pastranakd@gmail.com>
2021-11-16 09:58:30 +0800

    Second commit for market place

--- includes/constants.php
+++ includes/constants.php
@@ -3,16 +3,16 @@
 	ob_start();
 
     define('SITEURL','http://localhost/Market/');
-    define('LOCALHOST', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'mp_db');
+    // define('LOCALHOST', 'localhost');
+    // define('DB_USERNAME', 'root');
+    // define('DB_PASSWORD', '');
+    // define('DB_NAME', 'mp_db');
 
 
-    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    // $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-    if (!$conn){
-        die("Connection failed: " . mysqli_connect_error());
-    }
+    // if (!$conn){
+    //     die("Connection failed: " . mysqli_connect_error());
+    // }
 
 ?>
